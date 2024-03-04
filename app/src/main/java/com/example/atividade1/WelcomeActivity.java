8d17b39397d46ddee6ae682f48fa914dfa3ac512
joaothallys <joaothallys>
2024-03-04 20:50:17 -0300

    commit 1

---
+++ app/src/main/java/com/example/atividade1/WelcomeActivity.java
@@ -0,0 +1,26 @@
+package com.example.atividade1;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        Button buttonBack = findViewById(R.id.buttonBack);
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
